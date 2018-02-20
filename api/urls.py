3aac6c9816a384cb6584a4f4b1d4155192017485
VincentMagtalas <joseph.magtalas@gmail.com>
2018-02-20 11:46:28 +0800

    make the API index

--- api/urls.py
+++ api/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from api import views
 
 urlpatterns = [
-    url(r'^api/$', views.api_root),
-    url(r'^api/notes/$', views.NoteList.as_view(), name='note_list'),
-    url(r'^api/fbusers/$', views.FBUserList.as_view(), name='fbuser_list'),
-    url(r'^api/fbuser/(?P<pk>[0-9]+)/$', views.FBUserDetail.as_view(), name='fbuser_detail'),
-    url(r'^api/note/(?P<pk>[0-9]+)/$', views.NoteDetail.as_view(), name='note_detail'),
-    url(r'^api/fbusernotes/(?P<pk>[0-9]+)/$', views.FBUserNoteDetail.as_view(), name='fbuser_note'),
+    url(r'^$', views.api_root),
+    url(r'^notes/$', views.NoteList.as_view(), name='note_list'),
+    url(r'^fbusers/$', views.FBUserList.as_view(), name='fbuser_list'),
+    url(r'^fbuser/(?P<pk>[0-9]+)/$', views.FBUserDetail.as_view(), name='fbuser_detail'),
+    url(r'^note/(?P<pk>[0-9]+)/$', views.NoteDetail.as_view(), name='note_detail'),
+    url(r'^fbusernotes/(?P<pk>[0-9]+)/$', views.FBUserNoteDetail.as_view(), name='fbuser_note'),
 ]
